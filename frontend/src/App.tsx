01c171f756deaf2c9392c834f7ed430c9e6d1849
Eric Chukoltyansky <erik.chukoltyansky@gmail.com>
2024-05-03 16:35:04 +0300

    basic json object addition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,9 +13,3 @@ function App() {
 
 export default App;
 
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/EricChukoltyansky/Politico.git
-git push -u origin main
