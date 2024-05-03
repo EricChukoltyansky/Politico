a05017da22d4ed4803581cbf49c3b36a4b5b6e33
Eric Chukoltyansky <erik.chukoltyansky@gmail.com>
2024-05-03 14:58:53 +0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      
+    </>
+  );
+}
+
+export default App;
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/EricChukoltyansky/Politico.git
+git push -u origin main
